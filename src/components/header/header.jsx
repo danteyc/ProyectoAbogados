5e508dfbfdb59004f733580f1120e5a66071b864
rbeltrango <rbeltrann@gmail.com>
2022-01-31 23:56:20 -0500

    nav added

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,10 @@
 import "./header.scss"
-
+import { Nav } from "../nav/nav"
 export function Header(){
     return(
       <header>
         esto es un header
+        <Nav/>
       </header>
     );
   }
---
+++ src/components/nav/nav.jsx
@@ -0,0 +1,9 @@
+import "./nav.scss"
+
+export function Nav(){
+    return (
+        <nav>
+            <img width="200" src="https://laverdadnoticias.com/__export/1610641647967/sites/laverdad/img/2021/01/14/bts_se_convierte_en_la_imagen_oficial_de_coca_cola.jpg_63503792.jpg" alt="Imagen" />
+        </nav>
+    )
+}
