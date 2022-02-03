2a902dd11d12fa86095920b94855707791872827
Ronald <rbeltrann@gmail.com>
2022-02-02 22:11:27 -0500

    Lista de abogados axios

--- src/pages/PageListaAbogados/PageListaAbogados.jsx
+++ src/pages/PageListaAbogados/PageListaAbogados.jsx
@@ -1,6 +1,31 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import "./PageListaAbogados.scss"
 export function PageListaAbogados(){
+    const direccionApi = "https://61f426e510f0f7001768c855.mockapi.io/abogados";
+    
+    const[datosAbogados,SetDatosAbogados] = useState([]);
+    
+
+    
+    useEffect(()=> {
+        axios.get(direccionApi)
+            .then((respuesta)=>{
+                SetDatosAbogados(respuesta.data);
+//               console.log(respuesta.data);
+            })
+    },[]);
+
     return (
-        <div>Soy Vista PageListaAbogados</div>
+        <div>
+            {datosAbogados.map((abogado)=>(
+                <div>
+                    <h1>{abogado.nombre}</h1>
+                    <h2>{abogado.apellido}</h2>
+                    <h2>{abogado.empresa}</h2>
+                    <img src={abogado.foto} width="300"/>
+                </div>
+            ))}
+        </div>
     )
-    }
+}
