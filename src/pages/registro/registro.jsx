4c1afab211838e524a1f6b4aed15b48a14174a4e
NahiaEs <nahiaescalanteccoyllo@gmail.com>
2022-02-13 21:30:26 -0500

    avancedomingo

--- src/pages/registro/registro.jsx
+++ src/pages/registro/registro.jsx
@@ -1,5 +1,5 @@
 import "./registro.scss"
-import {Form, Button, Checkbox, DatePicker, Input, Select} from "antd";
+import {Form, Button, Checkbox, DatePicker, Input, Select, InputNumber} from "antd";
 
 export function PageRegistro(){
     // return (
@@ -10,58 +10,172 @@ export function PageRegistro(){
     //     </form>
     //     </>
     // )
+    function onChange(e) {
+      console.log(`checked = ${e.target.checked}`);
+    }
     return (
             <div>
             
-                    <Form labelCol={{ span: 5 }} wrapperCol={{ span: 14}}>
-                        <Form.Item name="NombreCompleto" label="Nombre Completo">
+                    <Form 
+                    autoComplete="off"
+                    labelCol={{ span: 6 }} 
+                    wrapperCol={{ span: 10}}
+                    onFinish={(values)=>{
+                      console.log({values});
+                    }}
+                    onFinishFailes={(error)=> {
+                      console.log({error});
+                    }}
+
+                    >
+                        <Form.Item 
+                        name="Nombres"
+                        label="Nombres" 
+                        rules={[
+                          {
+                            required:true,
+                            message: "Por favor ingresa tus nombres",
+                          }, 
+                          {whitespace: true},
+                          {min: 3},
+                        ]}
+                        hasFeedback
+                        >
                         <Input placeholder="Ingresa tus nombres"/>
                         </Form.Item>
 
-                        <Form.Item name="ApellidoCompleto" label="Apellido Completo">
+                        <Form.Item 
+                        name="Apellidos"
+                         label="Apellidos" rules={[
+                          {
+                            required:true,
+                            message: "Por favor ingresa tus apellidos",
+                          }, 
+                          {whitespace: true},
+                          {min: 3},
+                        ]}
+                        hasFeedback>
                         <Input placeholder="Ingresa tus apellidos"/>
                         </Form.Item>
 
-                        <Form.Item name="NumerodeColegiatura" label="Nro de colegiatura">
+                        <Form.Item name="Numero de Colegiatura" label="Nro de colegiatura" rules={[
+                          {
+                            required:true,
+                            message: "Por favor ingresa tu nro de colegiatura",
+                          }, 
+                          {whitespace: true},
+                          {min: 3},
+                        ]}
+                        hasFeedback>
                         <Input placeholder="Ingresa tu nro de colegiatura"/>
                         </Form.Item>
 
-                        <Form.Item name="DNI" label="DNI">
+                        <Form.Item name="DNI" label="DNI" rules={[
+                          {
+                            required:true,
+                            message: "Por favor ingresa tu nro de DNI",
+                          }, 
+                          {whitespace: true},
+                          {min: 3},
+                        ]}
+                        hasFeedback>
                         <Input placeholder="Ingresa tu nro de DNI"/>
                         </Form.Item>
 
-                        <Form.Item name="correoel" label="Correo electronico">
+                        <Form.Item name="correo" label="Correo electronico" 
+                        rules={[
+                          {
+                            required:true,
+                            message: "Por favor ingresa tu correo",
+                          }, 
+                          {type:"email"},
+                          {message: "Por favor ingrese un correo válido"},
+                        ]}
+                        hasFeedback>
                         <Input placeholder="Ingresa tu correo"/>
                         </Form.Item>
 
-                        <Form.Item name="genero" label="Genero">
-                          <Select placeholder= "Selecciona tu genero">
+                        <Form.Item name="genero" label="Genero" requiredMark="optional">
+                          <Select placeholder= "Selecciona tu género">
                            <Select.Option value="masculino">Masculino</Select.Option>
                            <Select.Option value="femenino">Femenino</Select.Option>
                            <Select.Option value="prefieronodecirlo">Prefiero no decirlo</Select.Option>
                           </Select>
                         </Form.Item>
                         
-                        <Form.Item name="contrasena" label="Contraseña">
+                        <Form.Item 
+                        name="contraseña"
+                        label="Contraseña"
+                        rules={[
+                          {
+                            required:true,
+                          }, 
+                          { min:6 },
+                        ]}
+                        hasFeedback>
+
                         <Input.Password placeholder="ingresa tu contraseña"/>
                         </Form.Item>
 
-                        <Form.Item name="confirmarconstrasena" label="Confirmar contraseña">
+                        <Form.Item 
+                        name="confirmar constraseña"
+                        label="Confirmar contraseña"
+                        dependencies={["contraseña"]}
+                        rules={[
+                          {
+                            required:true,
+                            message: "Por favor confirmar contraseña",
+                          }, 
+                          ({getFieldValue})=> ({
+                            validator (_,value){
+                              if(!value|| getFieldValue( "contraseña")=== value){
+                                return Promise.resolve ()
+                              }
+                              return Promise.reject( "Las constraseñas no coinciden")
+                            }
+                          })
+                        ]}
+                        hasFeedback>
+
                         <Input.Password placeholder="Ingresa tu contraseña"/>
                         </Form.Item>
 
-                        <Form.Item name="fechadenacimiento" label="Fecha de nacimiento">
+                        <Form.Item name="fecha de nacimiento" label="Fecha de nacimiento" 
+                        rules={[
+                          {
+                            required:true,
+                            message: "Por favor selecciona tu fecha de nacimiento"},
+                        ]}
+                        hasFeedback
+                        >
+                          
                         <DatePicker 
                         style={{ width: "100%"}}
                          picker="date" 
                          placeholder="Selecciona tu fecha de nacimiento"/>
                         </Form.Item>
 
-                        <Form.Item name="acuerdo">
-                        <Checkbox> Acepto los términos y condiciones <a href="#"></a></Checkbox> 
+                        <Form.Item 
+                        name="acuerdo" 
+                        wrapperCol={{ span: 24 }}
+                        valuePropName="checked"
+                        rules={[
+                          {
+                            validator: (_,value)=>
+                            value 
+                            ? Promise.resolve ()
+                            : Promise.reject("Para proceder con el registro debe aceptar nuestros términos y condiciones"),
+                          },
+                          {
+                            required:true,
+                            message: "Para proceder con el registro debe aceptar nuestros términos y condiciones"},
+                        ]}
+                        >
+                        <Checkbox onChange={onChange}>Acepto los  <a href="#">términos y condiciones</a></Checkbox>
                         </Form.Item>
 
-                        <Form.Item name="registrocompletado">
+
+                        <Form.Item  wrapperCol={{span: 24}} name="registrocompletado">
                         <Button type= "primary" htmlType= "submit">Registro</Button>
                         </Form.Item>
                         
